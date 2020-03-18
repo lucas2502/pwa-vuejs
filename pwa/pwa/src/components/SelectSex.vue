c4c058a1b671ffe17a2ccd96dcb382ab2565cf44
Lucas Gonçalves <lucas2502@live.com>
2020-03-18 14:06:26 -0300

    Create select sex

---
+++ pwa/pwa/src/components/SelectSex.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-row justify="center">
+    <v-col
+      cols="6"
+    >
+        <v-img v-model="selected" max-width="100px" class="item-select" src="../assets/man.png" title="Selecionar Homem" @click.prevent="select('man')"></v-img>
+    </v-col>
+
+    <v-col
+      cols="6"
+    >
+        <v-img v-model="selected" max-width="100px" class="item-select" src="../assets/woman.png" title="Selecionar mulher" @click.prevent="select('woman')"></v-img>
+    </v-col>
+
+  </v-row>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      selected: null
+    }
+  },
+  methods: {
+    select (value) {
+      this.selected = value
+      this.$emit('select-sex', this.selected)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.item-select {
+  cursor: pointer;
+  & :hover {
+    opacity: 0.5
+  }
+}
+</style>
---
+++ pwa/pwa/src/view/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container class="pa-4 text-center">
+    <v-row align="center" justify="center">
+      <v-col cols="12">
+        <v-img max-width="150px" src="../assets/logo.svg"></v-img>
+      </v-col>
+      <v-col cols="12">
+        <h4>Alimentação Saudável</h4>
+      </v-col>
+    </v-row>
+    <SelectSex @select-sex="selected"/>
+  </v-container>
+</template>
+
+<script>
+import SelectSex from '../components/SelectSex'
+
+export default {
+  components: {
+    SelectSex
+  },
+  data () {
+    return {
+      valueSelected: null
+    }
+  },
+  methos: {
+    selected (e) {
+      this.valueSelected = e
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
